7bce8ed4fcd946d9da4826adf00ba0d275297a14
gloriaJun <pureainu@gmail.com>
2018-04-14 14:33:14 +0900

    spring boot 프로젝트 생성

---
+++ backend/src/main/java/gloria/Application.java
@@ -0,0 +1,16 @@
+package gloria;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        logger.debug("Spring boot Application Start~!!");
+    }
+}
